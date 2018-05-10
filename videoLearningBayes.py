Fixed Linear SVC. Added Bayes, Decision Tree, Gradient Boosting, Nearest Neighbors, Linear Discriminant, MLPC.

videoLearningBayes.py
@@ -1,8 +1,7 @@
 import pandas
 import numpy as np
-from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 
 colnames = ['idVideo', 'numberOfViews', 'likes', 'dislikes', 'mistakes', 'presentation', 'informative', 'quality', 'numberOfSubscribers', 'averageViewsAllVideos',
             'likesToDislikes', 'likesDislikesDifference', 'viewsRatio']
@@ -10,38 +9,32 @@ data = pandas.read_csv('C:/Users/yulia/videodataWork.csv', usecols=colnames)
 surveyData = data[data['mistakes'].notnull()]
 surveyData.to_csv('C:/Users/yulia/surveyData.csv', sep=',')
 #surveyData.fillna(0)
-# print(np.any(np.isnan(surveyData)))
-# print(np.all(np.isfinite(surveyData)))
+#print(np.any(np.isnan(surveyData)))
+#print(np.all(np.isfinite(surveyData)))
 
 # print(len(surveyData))
-# choose size of training and test set
-trainingSet = surveyData.tail(n=70)
+
+trainingSet = surveyData.tail(n=80)
 testSet = surveyData.head(n=20)
 
-# choose columns for training set
+
 featureColumns = ['numberOfViews', 'likes', 'dislikes', 'numberOfSubscribers', 'averageViewsAllVideos', 'likesToDislikes', 'likesDislikesDifference', 'viewsRatio']
 X = trainingSet.loc[:, featureColumns]
-print(X.shape)
+#print(X.shape)
 
 tests = ['mistakes', 'informative', 'presentation', 'quality']
 for predData in tests:
     y = trainingSet.loc[:, predData]
-    print(y.shape)
+    #print(y.shape)
 
-    # clf = SVC()
-    # print(clf.fit(X, y))
-
-    clf = LinearSVC(random_state=0)
-    clf.fit(X, y)
-
+    gnb = GaussianNB()
+    gnb.fit(X, y)
 
     testSet = testSet.drop(predData, axis=1)
     XNew = testSet.loc[:, featureColumns]
     # print(XNew)
 
-
-    newPredClass = clf.predict(XNew)
-
+    newPredClass = gnb.predict(XNew)
 
     surveyDataMistakes = list(surveyData[predData].astype(int))
     tenFirst = surveyDataMistakes[:20]
@@ -56,8 +49,3 @@ for predData in tests:
     print(len([i for i, j in zip(newPredClass, tenFirst) if i == j])/len(testSet))
     print("\n")
 
-    # plt.scatter(tenFirst, newPredClass)
-    # plt.xlabel("True Values")
-    # plt.ylabel("Predictions")
-    #
-    # plt.show()

videoLearningDecisionTree.py
@@ -1,6 +1,6 @@
+from sklearn import tree
 import pandas
 import numpy as np
-from sklearn.svm import SVC, LinearSVC
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
@@ -9,13 +9,8 @@ colnames = ['idVideo', 'numberOfViews', 'likes', 'dislikes', 'mistakes', 'presen
 data = pandas.read_csv('C:/Users/yulia/videodataWork.csv', usecols=colnames)
 surveyData = data[data['mistakes'].notnull()]
 surveyData.to_csv('C:/Users/yulia/surveyData.csv', sep=',')
-#surveyData.fillna(0)
-# print(np.any(np.isnan(surveyData)))
-# print(np.all(np.isfinite(surveyData)))
 
-# print(len(surveyData))
-# choose size of training and test set
-trainingSet = surveyData.tail(n=70)
+trainingSet = surveyData.tail(n=80)
 testSet = surveyData.head(n=20)
 
 # choose columns for training set
@@ -31,7 +26,7 @@ for predData in tests:
     # clf = SVC()
     # print(clf.fit(X, y))
 
-    clf = LinearSVC(random_state=0)
+    clf = tree.DecisionTreeClassifier()
     clf.fit(X, y)
 
 
@@ -61,3 +56,4 @@ for predData in tests:
     # plt.ylabel("Predictions")
     #
     # plt.show()
+

videoLearningGradientBoosting.py
@@ -1,6 +1,6 @@
+from sklearn.ensemble import GradientBoostingClassifier
 import pandas
 import numpy as np
-from sklearn.svm import SVC, LinearSVC
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
@@ -9,13 +9,8 @@ colnames = ['idVideo', 'numberOfViews', 'likes', 'dislikes', 'mistakes', 'presen
 data = pandas.read_csv('C:/Users/yulia/videodataWork.csv', usecols=colnames)
 surveyData = data[data['mistakes'].notnull()]
 surveyData.to_csv('C:/Users/yulia/surveyData.csv', sep=',')
-#surveyData.fillna(0)
-# print(np.any(np.isnan(surveyData)))
-# print(np.all(np.isfinite(surveyData)))
 
-# print(len(surveyData))
-# choose size of training and test set
-trainingSet = surveyData.tail(n=70)
+trainingSet = surveyData.tail(n=80)
 testSet = surveyData.head(n=20)
 
 # choose columns for training set
@@ -31,14 +26,13 @@ for predData in tests:
     # clf = SVC()
     # print(clf.fit(X, y))
 
-    clf = LinearSVC(random_state=0)
+    clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
     clf.fit(X, y)
 
 
     testSet = testSet.drop(predData, axis=1)
     XNew = testSet.loc[:, featureColumns]
     # print(XNew)
-
 
     newPredClass = clf.predict(XNew)
 

videoLearningKnn.py
@@ -1,8 +1,7 @@
 import pandas
 import numpy as np
-from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 
 colnames = ['idVideo', 'numberOfViews', 'likes', 'dislikes', 'mistakes', 'presentation', 'informative', 'quality', 'numberOfSubscribers', 'averageViewsAllVideos',
             'likesToDislikes', 'likesDislikesDifference', 'viewsRatio']
@@ -10,15 +9,15 @@ data = pandas.read_csv('C:/Users/yulia/videodataWork.csv', usecols=colnames)
 surveyData = data[data['mistakes'].notnull()]
 surveyData.to_csv('C:/Users/yulia/surveyData.csv', sep=',')
 #surveyData.fillna(0)
-# print(np.any(np.isnan(surveyData)))
-# print(np.all(np.isfinite(surveyData)))
+print(np.any(np.isnan(surveyData)))
+print(np.all(np.isfinite(surveyData)))
 
 # print(len(surveyData))
-# choose size of training and test set
-trainingSet = surveyData.tail(n=70)
+
+trainingSet = surveyData.tail(n=80)
 testSet = surveyData.head(n=20)
 
-# choose columns for training set
+
 featureColumns = ['numberOfViews', 'likes', 'dislikes', 'numberOfSubscribers', 'averageViewsAllVideos', 'likesToDislikes', 'likesDislikesDifference', 'viewsRatio']
 X = trainingSet.loc[:, featureColumns]
 print(X.shape)
@@ -28,20 +27,14 @@ for predData in tests:
     y = trainingSet.loc[:, predData]
     print(y.shape)
 
-    # clf = SVC()
-    # print(clf.fit(X, y))
-
-    clf = LinearSVC(random_state=0)
-    clf.fit(X, y)
-
+    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(X, y)
 
     testSet = testSet.drop(predData, axis=1)
     XNew = testSet.loc[:, featureColumns]
     # print(XNew)
 
-
-    newPredClass = clf.predict(XNew)
-
+    newPredClass = neigh.predict(XNew)
 
     surveyDataMistakes = list(surveyData[predData].astype(int))
     tenFirst = surveyDataMistakes[:20]
@@ -55,9 +48,3 @@ for predData in tests:
     print(len(testSet))
     print(len([i for i, j in zip(newPredClass, tenFirst) if i == j])/len(testSet))
     print("\n")
-
-    # plt.scatter(tenFirst, newPredClass)
-    # plt.xlabel("True Values")
-    # plt.ylabel("Predictions")
-    #
-    # plt.show()